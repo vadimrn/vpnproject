821ecec6540770943185d9b02d9a659cff24445e
vadimrn <120738327+vadimrn@users.noreply.github.com>
2024-02-02 13:24:39 +0500

    Initial Commit

---
+++ vpnproject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  vpnproject
+//
+//  Created by Lala on 02.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
